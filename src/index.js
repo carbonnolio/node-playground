474f7c99072d1f1cb0f924b3f20794a0bf35021b
carbonnolio <ynblpek83@gmail.com>
2017-06-24 16:43:27 -0400

    os demo

--- src/index.js
+++ src/index.js
@@ -1,4 +1,8 @@
-
+import os from 'os';
 /*eslint-disable no-console*/
 
-console.log('Hello World!');
+const toMb = (f) => Math.round((f / 1024 / 1024) * 100) / 100;
+
+console.log(`Host OS: ${os.hostname()}`);
+console.log(`15 min load avg: ${os.loadavg()[2]}`);
+console.log(`${toMb(os.freemem())} of ${toMb(os.totalmem())} Mb free.`);
